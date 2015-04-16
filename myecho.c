4cdb92b0bacfd64ec61227dee9cb83eb94703698
SteMaBa <stefan.kaki@web.de>
2015-04-16 21:03:53 +0200

    Loesung Uebung01 SteMaBa

--- myecho.c
+++ myecho.c
@@ -2,8 +2,22 @@
 
 int main(int argc, char* argv[], char* envp[]) 
 {
+  int iZaehler = 1;
+  if (argv[1] == 0)
+  {
+     printf("\n");
+     return 0;			//Keine Argumente, leere Zeile ausgeben
+  }
+  else 
+  {
+     while (iZaehler < argc)
+     {
+       printf("%s ",argv[iZaehler]);	//Argumente ausgeben
+       iZaehler++;
+     }
+   }
 
-  printf("Test");  
+  printf("\n");  
 
   return 0;
 }
